82a8885365fee9cb5de4ee089f961abc7471d37f
DarkPhoenix2704 <anbarasun123@gmail.com>
2026-05-08 12:04:02 +0000

    feat: undo redo framework

--- packages/nocodb/src/meta/meta.service.ts
+++ packages/nocodb/src/meta/meta.service.ts
@@ -156,6 +156,7 @@ export class MetaService {
       [MetaTable.DATE_DEPENDENCY]: 'dd',
       [MetaTable.API_TOKEN_SCOPES]: 'ats',
       [MetaTable.TRASH]: 'tr',
+      [MetaTable.OPERATION_LOGS]: 'opl',
     };
 
     const prefix = prefixMap[target] || 'nc';
